fa05f1f1b972cc7710d2d4ca332ef92c9f389546
Eunice Chou <eunice@flavorize.com>
2012-02-07 16:44:49 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'bfc3a793fcc0e582bf70349870eb6e14d3284b30293b67780adedf68cc330197b20758e96262ea7a6cc3ca90eeb08913b4c5b206aff6702e7732389065f12dc7'
